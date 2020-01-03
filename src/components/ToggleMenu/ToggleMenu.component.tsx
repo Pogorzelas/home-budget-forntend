ea3357bcd1149998255bee014cc0e6e7749f8885
Tomassz <tomasz.pogorzelski@neoteric.eu>
2020-01-04 00:14:38 +0100

    chore(ToggleMenu): add hook to manage menu

--- src/components/ToggleMenu/ToggleMenu.component.tsx
+++ src/components/ToggleMenu/ToggleMenu.component.tsx
@@ -1,10 +1,12 @@
 import {IconButton} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import React, {FC} from "react";
+import {useToggleMenu} from "./useToggleMenu";
 
 export const ToggleMenu: FC = () => {
+  const {handleMenu} = useToggleMenu();
   return (
-    <IconButton>
+    <IconButton onClick={handleMenu}>
       <MenuIcon/>
     </IconButton>
   );
---
+++ src/components/ToggleMenu/useToggleMenu.ts
@@ -0,0 +1,18 @@
+import {useDispatch} from 'react-redux';
+import {dialogActions} from "../../store/dialog/action";
+
+interface Hook {
+  handleMenu: VoidFunction;
+}
+
+export const useToggleMenu = (): Hook => {
+  const dispatch = useDispatch();
+
+  const handleMenu = (): void => {
+    dispatch(dialogActions.toggleMenu())
+  };
+
+  return {
+    handleMenu
+  }
+};
