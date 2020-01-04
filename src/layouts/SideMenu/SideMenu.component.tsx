57b8135243dcbd4d27eec161506074759eefcecc
Tomassz <tomasz.pogorzelski@neoteric.eu>
2020-01-04 11:59:06 +0100

    chore(SideMenu): add method to manage link path

--- src/layouts/SideMenu/SideMenu.component.tsx
+++ src/layouts/SideMenu/SideMenu.component.tsx
@@ -3,15 +3,10 @@ import {Drawer} from "@material-ui/core";
 import {useSideMenu} from "./useSideMenu";
 
 export const SideMenu: FC = () => {
-  const {isOpen, handleClose} = useSideMenu();
+  const {isOpen, handleClose, handleLink} = useSideMenu();
   return (
     <Drawer open={isOpen} onClose={handleClose}>
-      <h1>Mock</h1>
-      <h2>Mock</h2>
-      <h3>Mock</h3>
-      <h4>Mock</h4>
-      <h5>Mock</h5>
-      <h6>Mock</h6>
+
     </Drawer>
   );
 };
--- src/layouts/SideMenu/useSideMenu.ts
+++ src/layouts/SideMenu/useSideMenu.ts
@@ -1,9 +1,11 @@
 import {useDispatch, useSelector} from "react-redux";
 import {dialogActions} from "../../store/dialog/action";
+import {useHistory} from "react-router";
 
 interface Hook {
   isOpen: boolean;
   handleClose: VoidFunction;
+  handleLink: (path: string) => void;
 }
 
 interface State {
@@ -15,13 +17,20 @@ interface State {
 export const useSideMenu = (): Hook => {
   const isOpen = useSelector((state: State): boolean => state.dialog.isSideMenuOpened);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const handleClose = (): void => {
     dispatch(dialogActions.toggleMenu());
   };
 
+  const handleLink = (path: string): void => {
+    handleClose();
+    history.push(path);
+  };
+
   return {
     isOpen,
     handleClose,
+    handleLink
   }
 };
