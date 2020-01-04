2cc83ed8a90bfe55c78430ba14c87e4f93a28eff
Tomassz <tomasz.pogorzelski@neoteric.eu>
2020-01-04 13:25:54 +0100

    feat(theme): save theme type in local storage

--- src/store/theme/reducer.ts
+++ src/store/theme/reducer.ts
@@ -1,13 +1,16 @@
 import {initialState, ThemeState} from "./state";
 import {ThemeAction} from "./action";
+import {LocalStorageKey} from "../../enums/LocalStorageKey.enum";
 
 export default (state = initialState, action: ThemeAction): ThemeState => {
 
   switch (action.type) {
     case 'Theme.SetTheme':
+      const type = state.type === 'light' ? 'dark' : 'light';
+      localStorage.setItem(LocalStorageKey.ThemeType, type);
       return {
         ...state,
-        type: state.type === 'light' ? 'dark' : 'light'
+        type
       };
     default:
       return state;
--- src/store/theme/state.ts
+++ src/store/theme/state.ts
@@ -1,9 +1,12 @@
 import {PaletteType} from "@material-ui/core";
+import {LocalStorageKey} from "../../enums/LocalStorageKey.enum";
 
 export interface ThemeState {
   type: PaletteType;
 }
 
+const SavedTheme = localStorage.getItem(LocalStorageKey.ThemeType) as PaletteType;
+
 export const initialState: ThemeState = {
-  type: 'dark',
+  type: SavedTheme || 'dark',
 };
