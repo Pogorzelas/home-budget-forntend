528f5f8d431f388ec8f65eced425e777901a00b2
Tomassz <tomasz.pogorzelski@neoteric.eu>
2020-01-14 22:59:45 +0100

    fix(Income): add missin import

--- src/view/Income/index.tsx
+++ src/view/Income/index.tsx
@@ -1,7 +1,7 @@
 import React, {FC} from "react";
 import IncomeManager from "./IncomeManager/index";
 import {useIncome} from "./useIncome";
-import Loader from "../../components/Loader";
+import Loader from "../../components/Loader/index";
 
 const Income: FC = () => {
   const isLoading = useIncome();
